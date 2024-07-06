4d0c229f738614924d6c9070f14cd5dcd1ec073d
Illia Tarasenko <99745038+RedSSD@users.noreply.github.com>
2024-07-06 12:46:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python3 manage.py makemigrations authentication
+python3 manage.py makemigrations articles
+python3 manage.py migrate
 service cron start
 python3 manage.py crontab add
 python3 manage.py runserver 0.0.0.0:8000
